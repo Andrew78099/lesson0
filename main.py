f825f7a6359ffa848f5ef02a2e23cc3835df3567
Andrew78099 <andydm78@mail.ru>
2024-07-01 21:03:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#1st program
+print(9 ** 0.5 * 5)
+
+#2nd program
+print(bool(9.99>9.98 and 1000!=1000.1))
+
+#3nd program
+a=int(1234)
+b=int(5678)
+a1=a%1000
+a=a1//10
+b1=b%1000
+b=b1//10
+print(a+b)
+
+#4nd program
+ch1=13.42
+ch2=42.13
+c1=int(ch1)
+c2=int(ch2)
+d1=int(ch1*100%100)
+d2=int(ch2*100%100)
+print(bool(c1 == d2 or c2 == d1))
