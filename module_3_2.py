822cc9b4e37ea7d7d0ba1828223f2228925517ee
Andrew78099 <andydm78@mail.ru>
2024-07-17 14:11:51 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -8,17 +8,17 @@ def send_email(message, recipient, *, sender = "university.help@gmail.com"):
             flag = False
             continue
     if flag == False:
-        print("Невозможно отправить письмо с адреса <sender> на адрес <recipient>")
+       print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
     elif recipient[:-5:-1] != "moc." and recipient[:-4:-1] != "ur." and recipient[:-5:-1] != "ten.":
-        print(f'"Невозможно отправить письмо с адреса {sender} "на адрес" {recipient}.')
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
     elif sender[:-5:-1] != "moc." and sender[:-4:-1] != "ur." and recipient[:-5:-1] != "ten.":
-        print(f'"Невозможно отправить письмо с адреса {sender} "на адрес" {recipient}.')
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
     elif sender == recipient:
         print("Нельзя отправить письмо самому себе!")
     elif sender == "university.help@gmail.com":
-        print(f'"Письмо успешно отправлено с адреса" {sender} "на адрес" {recipient}.')
+        print(f'Письмо успешно отправлено с адреса" {sender} на адрес {recipient}.')
     else:
-        print(f'"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
 
 send_email('Это сообщение для проверки связи', 'vasyok1337gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru',
