0f8747821da6eea276f46d9085d6c7a421558da0
Andrew78099 <andydm78@mail.ru>
2024-07-18 23:33:37 +0300

    Update homework_2_hard.py

--- homework_2_hard.py
+++ homework_2_hard.py
@@ -1,17 +1,19 @@
 def prov_del(n):
     seq = []
     for j in range(1,20):
-        for i in range(1,20,1):
+        for i in range(1,20):
             s = j + i
             if n % s == 0 and j < i:
-                if j != 0:
-                    seq.append(j)
-                if i != 0:
-                    seq.append(i)
+                seq.append(j)
+                seq.append(i)
     result = str()
     for b in seq:
         result = result + str(b)
     print("Результат:",result)
 n = input("Введите число от 3 до 20:")
 n = int(n)
+print(n)
+while n < 3 or n > 20:
+    n = input("Введите другое число:")
+    n = int(n)
 prov_del(n)
