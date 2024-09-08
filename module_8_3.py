6f54c2833a88bed7ca6e3c42fd11ef72cd720abd
Andrew78099 <andydm78@mail.ru>
2024-09-08 23:55:32 +0300

    Update module_8_3.py

--- module_8_3.py
+++ module_8_3.py
@@ -1,25 +1,21 @@
-class Car():
+class Car:
     def __init__(self, model, __vin, __numbers):
         self.model = model
         self.__vin = __vin
         self.__numbers = __numbers
 
-
-
     def __is_valid_vin(vin_number):
-        if vin_number.isinstance(int) and 1000000 <= vin_number <=  9999999:
+        print(vin_number)
+        if isinstance(vin_number, int) and 1000000 <= vin_number <= 9999999:
             return True
         else:
             raise IncorrectVinNumber('Некорректный тип vin номер')
 
-
     def __is_valid_numbers(numbers):
-            if numbers.isinstance(str) and len(numbers) == 6:
-                return True
-            else:
+        if isinstance(numbers, str) and len(numbers) == 6:
+            return True
+        else:
                 raise IncorrectCarNumbers('Неверная длина номера')
-
-
 
 
 class IncorrectVinNumber(Exception):
